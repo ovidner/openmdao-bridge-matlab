9565077093e75761dcae089416d95ad2a6eb50b7
Olle Vidner <olle@vidner.se>
2020-12-11 01:05:06 +0100

    Use setuptools-scm

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import find_packages, setup
 
 setup(
     name="openmdao-bridge-matlab",
-    version="0.0.0",
+    use_scm_version=True,
     author="Olle Vidner",
     author_email="olle@vidner.se",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     python_requires=">=3.6, <4",
     install_requires=["openmdao"],
+    setup_requires=["setuptools_scm"],
 )
